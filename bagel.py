2fb2f02303febda7960ed1a6e3d4a8d273ba296a
AdkhamSec <makhmudjonovbboy@gmail.com>
2023-08-15 00:36:42 +0900

    Rewritten
    Completely rewrote the code by myself to understand it deeply

--- bagel.py
+++ bagel.py
@@ -1,71 +1,68 @@
-import random
-DIGITS  = 2 
+from random import randint
+
+DIGITS = 3
 MAX_GUESSES = 10
 
 def main():
-    print('''Bagels, adeductive logic game.
-          I am thinking of a {}-digit number with no repeated digits. Try to guess what it is. Here are some clues: 
+    print("""Bagels, adeductive logic game.
+          I am thinking of a """ + str(DIGITS) + '''-digit number with no repeated digits. Try to guess what it is. Here are some clues: 
           When I say:    That means: 
           Pico           One digit is correct but in the wrong position. 
           Fermi          One digit is correct and in the right position. 
           Bagels         No digit is correct. 
           
-          For example, if the secret number was 248 and your guess was 843, the clues would be Fermi Pico.'''.format(DIGITS))
+          For example, if the secret number was 248 and your guess was 843, the clues would be Fermi Pico.''')
     
     while True:
         secretNum = getSecretNum()
+
         print('I have thought up a number.')
-        print(' You have {} guesses to get it .'.format(MAX_GUESSES))
+        print('You have ' + str(MAX_GUESSES) + ' gueses to get it.')
 
         numGuesses = 1
         while numGuesses <= MAX_GUESSES:
             guess = ''
             while len(guess) != DIGITS or not guess.isdecimal():
-                print("Guess #{}: ".format(numGuesses))
+                print("Guess #"+str(numGuesses)+':')
                 guess = input('> ')
-            
+
             clues = getClues(guess, secretNum)
             print(clues)
-            numGuesses += 1
+            numGuesses +=1
 
             if guess == secretNum:
                 break
-            if numGuesses > MAX_GUESSES:
-                print("You ran out of gueses.")
-                print("The answer was {}.".format(secretNum))
             
-            print("Do you want to play agay? (yes or no)")
-            if not input('> ').lower().startswith('y'):
-                break
-        print("Thanks for playing!")
+            if int(numGuesses) > int(MAX_GUESSES):
+                print('You ran out of guesses.')
+                print('The answer was ' + secretNum)
 
+        print("Do you want to play agan? (yes or no)")
+        if  not input('> ').lower().startswith('y'):
+            break
+    print('Thanks for playing!') 
+            
 
 def getSecretNum():
-    numbers = list('0123456789')
-    random.shuffle(numbers)
-
-    # Get the firs DIGITS digits in the list for the secret number:
-    secretNum = ''
-    for i in range(DIGITS):
-        secretNum += str(numbers[i])
-    return secretNum
+    secretNumber = str(randint(100, 999))
+    return secretNumber
 
 def getClues(guess, secretNum):
     if guess == secretNum:
-        return "You Got it!"
+        print('You get it!')
     
     clues = []
-
-    for i in range(len(guess)):
-        if  guess[i] == secretNum[i]:
+    for i in range (len(guess)):
+        if guess[i] == secretNum[i]:
             clues.append('Fermi')
         elif guess[i] in secretNum:
             clues.append('Pico')
     if len(clues) == 0:
-        return "Bagels"
+        return 'Bagels'
     else:
         clues.sort()
         return ''.join(clues)
-    
+
+
 if __name__ == '__main__':
     main()
